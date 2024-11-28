1e9f7ac61cec59566d4b0c10c872ec85935bcb59
StrandedSoftwareDeveloper <47873522+StrandedSoftwareDeveloper@users.noreply.github.com>
2024-11-28 04:06:07 -0500

    Hopefully make it work on 0.13.0

--- build.zig
+++ build.zig
@@ -16,13 +16,13 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     // Make module available as dependency. (borrowed/adapted from https://github.com/veloscillator/zig-wav/blob/69ef67061fc9ecb87d0b3d21287701aefae2ec09/build.zig#L9C1-L9C1)
-    _ = b.addModule("genAlg", .{ .root_source_file = .{ .cwd_relative = "src/zigGenAlg.zig" } });
+    _ = b.addModule("genAlg", .{ .root_source_file = b.path("src/zigGenAlg.zig") });
 
     const lib = b.addStaticLibrary(.{
         .name = "genAlg",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .cwd_relative = "src/zigGenAlg.zig" },
+        .root_source_file = b.path("src/zigGenAlg.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -53,7 +53,7 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .cwd_relative = "src/zigGenAlg.zig" },
+        .root_source_file = b.path("src/zigGenAlg.zig"),
         .target = target,
         .optimize = optimize,
     });
